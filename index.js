b13893098c7aa569603eea9606209a1568939bdd
norayr333 <norayr.sahakyan1.y@tumo.org>
2020-01-25 00:46:50 +0400

    avelacrelem 24.01

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 var express = require("express");
 var app = express();
 
-app.get("/shlorikompot", function(req, res){
-   res.send("Hello shlorikompot");
+app.get("/", function(req, res){
+   res.send("<h1>Hello world</h1>");
 });
-app.get("/name/:name", function(req, res){
+app.get("/google/:search", function(req, res){
    var name = req.params.name;
-   res.send("<h1>Hello " + name +"</h1>");
-
+   var search = req.params.search;
+   
+   res.redirect(302,'https://www.google.com/search?q='+ search);
+   
+   
+});
 app.listen(3000, function(){
    console.log("Example is running on port 3000");
 });
